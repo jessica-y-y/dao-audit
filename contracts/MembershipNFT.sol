5a9a826ea31d5f1f9db24ded265a591dbbd28486
Jéssica Yule <jyulesc@gmail.com>
2026-04-24 17:11:53 -0300

    Update MembershipNFT.sol
    Revisão após auditoria Slither

--- contracts/MembershipNFT.sol
+++ contracts/MembershipNFT.sol
@@ -1,25 +1,35 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.25;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
-contract MembershipNFT is ERC721, Ownable {
+contract MembershipNFT is ERC721, Ownable, ReentrancyGuard {
 
     uint256 public tokenIdCounter;
     uint256 public totalMembers;
+
     mapping(address => bool) public hasNFT;
-    event MemberMinted(address indexed user);
+
+    event MemberMinted(address indexed user, uint256 tokenId);
 
     constructor() ERC721("DAOsubp Membership", "DSPM") Ownable(msg.sender) {}
 
-    function mint(address to) external onlyOwner {
+    function mint(address to) external onlyOwner nonReentrant {
+        //Checagem
+        require(to != address(0), "Invalid address");
         require(!hasNFT[to], "Already has NFT");
 
-        tokenIdCounter++;
-        _safeMint(to, tokenIdCounter);
+        // Efeitos (ANTES da interação externa)
         hasNFT[to] = true;
-         totalMembers++;
+        tokenIdCounter++;
+        uint256 newTokenId = tokenIdCounter;
+        totalMembers++;
+
+        // Interações (por último)
+        _safeMint(to, newTokenId);
+
+        emit MemberMinted(to, newTokenId);
     }
-    
 }
